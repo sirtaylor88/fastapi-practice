fb3cd6f37ab33c9b5baaffd9b4c5516036a8b009
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2025-03-21 04:47:28 +0100

    ✨ Add CRUD actions for users.

--- fastapi_practice/db/db_user.py
+++ fastapi_practice/db/db_user.py
@@ -19,3 +19,35 @@ def create_user(db: Session, request: UserBase) -> DbUser:
     db.refresh(new_user)  # to set new ID for the user.
 
     return new_user
+
+
+def get_users(db: Session) -> list[DbUser]:
+    """Get all users in DB."""
+    return db.query(DbUser).all()
+
+
+def get_user(db: Session, user_id: int) -> DbUser:
+    """Get an user by ID."""
+    return db.query(DbUser).filter(DbUser.id == user_id).first()
+
+
+def update_user(db: Session, user_id: int, request: UserBase) -> str:
+    """Update an user."""
+    user = db.query(DbUser).filter(DbUser.id == user_id)
+    user.update(
+        {
+            DbUser.username: request.username,
+            DbUser.email: request.email,
+            DbUser.password: request.password,
+        }
+    )
+    db.commit()
+    return "OK"
+
+
+def delete_user(db: Session, user_id: int) -> str:
+    """Delete an user."""
+    user = db.query(DbUser).filter(DbUser.id == user_id)
+    user.delete()
+    db.commit()
+    return "OK"
--- fastapi_practice/routers/user.py
+++ fastapi_practice/routers/user.py
@@ -5,12 +5,37 @@ from sqlalchemy.orm import Session
 
 from fastapi_practice.db import db_user
 from fastapi_practice.db.database import get_db
+from fastapi_practice.db.models import DbUser
 from fastapi_practice.schemas import UserBase, UserDisplay
 
-router = APIRouter(prefix="/user", tags=["user"])
+router = APIRouter(prefix="/users", tags=["user"])
 
 
 @router.post("/new", response_model=UserDisplay)
-def create_user(request: UserBase, db: Session = Depends(get_db)):
+def create_user(request: UserBase, db: Session = Depends(get_db)) -> DbUser:
     """Create a new user."""
     return db_user.create_user(db, request)
+
+
+@router.get("/", response_model=list[UserDisplay])
+def get_users(db: Session = Depends(get_db)) -> list[DbUser]:
+    """Retrieve all users."""
+    return db_user.get_users(db)
+
+
+@router.get("/{user_id}", response_model=UserDisplay)
+def get_user(user_id: int, db: Session = Depends(get_db)) -> DbUser:
+    """Retrieve an user."""
+    return db_user.get_user(db, user_id)
+
+
+@router.post("/{user_id}/update")
+def update_user(user_id: int, request: UserBase, db: Session = Depends(get_db)) -> str:
+    """Update an user."""
+    return db_user.update_user(db, user_id, request)
+
+
+@router.post("/delete/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)) -> str:
+    """Delete an user."""
+    return db_user.delete_user(db, user_id)
